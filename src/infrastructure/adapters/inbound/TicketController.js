cb8cacb8150ff2bc36ff064a7ac94bfb6f894e2f
andrecodelima <andrelimag89@gmail.com>
2024-07-01 22:37:08 -0300

    rotas principais feitas

--- src/infrastructure/adapters/inbound/TicketController.js
+++ src/infrastructure/adapters/inbound/TicketController.js
@@ -1,5 +1,7 @@
 import { openDb } from "../../config/configDb.js";
 
+
+// GET ALL
 export async function getAllTicket(req, resp){
     // const ticket = req.body
     try{
@@ -27,14 +29,17 @@ export async function getAllTicket(req, resp){
     }
 }
 
+// INSERT TICKET
 export async function insereTicket(req, resp){
    
     try{
         const ticket = req.body
         const db = await openDb()
         await db.run(
-            'INSERT INTO ticket (nome_evento, tipo) VALUES(?,?)',
-            [ticket.nome_evento, ticket.tipo]
+            'INSERT INTO ticket (nome_evento, artista, data_evento, horario, local_evento, setor)' + 
+            'VALUES(?,?,?,?,?,?)',
+            [ticket.nome_evento, ticket.artista, ticket.data_evento, 
+             ticket.horario, ticket.local_evento, ticket.setor]
         )
 
         console.log('Registro adicionado com sucesso')
@@ -46,11 +51,76 @@ export async function insereTicket(req, resp){
         )
 
     }catch(e){
-        console.error('Erro ao inserir registro' + e)
+        console.error(`Erro ao inserir registro -- ${e}`)
         resp.status(500).json(
             {
                 "statusCode":500,
                 "message":'Erro ao inserir registro'
+            }
+        )
+    }
+}
+
+// UPDATE TICKET
+export async function updateTicket(req, resp) {
+    try {
+        const ticket = req.body;
+        const db = await openDb();
+        await db.run(
+            'UPDATE ticket SET '  +
+            'nome_evento=?, '     +
+            'artista=?, '         +
+            'data_evento=?, '     +
+            'horario=?, '         +
+            'local_evento=?, '    +
+            'setor=? '            +
+            'WHERE id=?',
+
+            [ticket.nome_evento, ticket.artista, ticket.data_evento, 
+            ticket.horario, ticket.local_evento, ticket.setor, ticket.id]
+        );
+
+        console.log('Registro alterado com sucesso')
+        resp.json(
+            {
+                "statusCode":200,
+                "message":'Registro alterado com sucesso'
+            }
+        )
+    }catch(e){
+        console.error(`Erro ao atualizar registro -- ${e}`)
+        resp.status(500).json(
+            {
+                "statusCode":500,
+                "message":'Erro ao atualizar registro'
+            }
+        )
+    }
+}
+
+// REMOVE TICKET
+export async function removeTicket(req, resp){
+    try{
+        const id = req.body.id;
+        const db = await openDb();
+        await db.run(
+
+            'DELETE FROM ticket WHERE id=?', [id]
+        )
+        console.log('Registro removido com sucesso')
+        resp.json(
+            {
+                "statusCode":200,
+                "message":'Registro removido com sucesso'
+            }
+        )
+
+    }catch(e){
+        console.log(`Erro ao remover registro -- ${e}`)
+        resp.json(
+            {
+                "statusCode":500,
+                "message":`Erro ao remover registro`
             }
         )
     }
--- src/infrastructure/adapters/inbound/router.js
+++ src/infrastructure/adapters/inbound/router.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { insereTicket, getAllTicket } from "./TicketController.js";
+import { insereTicket, getAllTicket, updateTicket, removeTicket } from "./TicketController.js";
 
 const router = Router()
 
 router.get('/',(req, resp)=> resp.status(200).send('Home Ticket'))
 router.get('/ticket', getAllTicket)
 router.post('/ticket', insereTicket)
+router.put('/ticket', updateTicket)
+router.delete('/ticket', removeTicket)
 
 export default router
